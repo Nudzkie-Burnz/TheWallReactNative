c248ab69b6406402eef8a5ef9493539301d68953
Nudzkie-Burnz <stanbernienudo@gmail.com>
2024-07-16 16:29:29 +0800

    Create login unit test

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -1,5 +1,9 @@
+
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import LogInScreen from "../app/index";
+
 describe('LogIn', () => {
     it('should allow the user to login', () => {
-        
+        render(<LogInScreen/>)
     });
 })
